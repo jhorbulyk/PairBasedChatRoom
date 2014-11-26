2f7dd5b2acf0572bc3230a5494b6c3e0c2345bad
s24luo <simonluo732@hotmail.com>
2014-11-27 01:13:27 +0900

    Change Category to Categories

--- DatabaseInitializationScript.sql
+++ DatabaseInitializationScript.sql
@@ -21,19 +21,21 @@ CREATE TABLE User (
     username CHAR(20) NOT NULL UNIQUE
 );
 
-CREATE TABLE `pairbasedchatroom`.`Category` (
+#Create Categories Table
+CREATE TABLE `pairbasedchatroom`.`Categories` (
   `uuid` INT NOT NULL,
   `categoryName` TINYTEXT NOT NULL,
   PRIMARY KEY (`uuid`),
   UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC));
 
-ALTER TABLE `pairbasedchatroom`.`Category` 
+#Add foreign key for parentCategory
+ALTER TABLE `pairbasedchatroom`.`Categories` 
 ADD COLUMN `parentCategory` INT NULL AFTER `categoryName`,
 ADD INDEX `parentCategory_idx` (`parentCategory` ASC);
-ALTER TABLE `pairbasedchatroom`.`Category` 
+ALTER TABLE `pairbasedchatroom`.`Categories` 
 ADD CONSTRAINT `parentCategory`
   FOREIGN KEY (`parentCategory`)
-  REFERENCES `pairbasedchatroom`.`Category` (`uuid`)
+  REFERENCES `pairbasedchatroom`.`Categories` (`uuid`)
   ON DELETE CASCADE
   ON UPDATE CASCADE;
   
