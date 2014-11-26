51eb2827c10f95e60bd1cfc3a5eab6e668a6c37e
s24luo <simonluo732@hotmail.com>
2014-11-27 01:06:29 +0900

    CreateTopic

--- DatabaseInitializationScript.sql
+++ DatabaseInitializationScript.sql
@@ -21,3 +21,22 @@ CREATE TABLE User (
     username CHAR(20) NOT NULL UNIQUE
 );
 
+#Create Topic Table
+CREATE TABLE `pairbasedchatroom`.`Topic` (
+  `uuid` INT NOT NULL,
+  `topicTitle` TINYTEXT NOT NULL,
+  `positionAStatement` TINYTEXT NOT NULL,
+  `positionBStatement` TINYTEXT NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC));
+  
+#Add Foreign Key
+ALTER TABLE `pairbasedchatroom`.`Topic` 
+ADD COLUMN `category.uuid` INT NULL AFTER `positionBStatement`,
+ADD INDEX `category.uuid_idx` (`category.uuid` ASC);
+ALTER TABLE `pairbasedchatroom`.`Topic` 
+ADD CONSTRAINT `category.uuid`
+ FOREIGN KEY (`category.uuid`)
+ REFERENCES `pairbasedchatroom`.`Category` (`uuid`)
+ ON DELETE NO ACTION
+ ON UPDATE NO ACTION;
