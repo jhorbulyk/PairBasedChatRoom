3b874ef2e7b2883d0b39d95c6e89adfd6b50fb19
jhorbulyk <jhorbulyk@inbox.com>
2014-11-27 01:59:05 +0900

    Start working on triggers for user table.

--- DatabaseInitializationScript.sql
+++ DatabaseInitializationScript.sql
@@ -14,10 +14,60 @@ USE PairBasedChatRoom;
 #######################################################
 
 # Create the USERS table
-CREATE TABLE User (
-    uuid INT NOT NULL UNIQUE PRIMARY KEY,
+CREATE TABLE Users (
+    uuid BIGINT NOT NULL UNIQUE PRIMARY KEY,
     email CHAR(20) NOT NULL UNIQUE,
     password CHAR(20) NOT NULL,
     username CHAR(20) NOT NULL UNIQUE
 );
 
+# Triggers to handle integrety constraints on updates and insertions
+# Creation
+DELIMITER //
+CREATE TRIGGER NewUser BEFORE INSERT ON Users
+FOR EACH ROW
+BEGIN 
+    # Generate UUID if not created
+    IF (NEW.uuid = 0) THEN
+        SET NEW.uuid = UUID_SHORT();
+    END IF;
+    # Reject empty passwords
+    If (LENGTH(NEW.password) = 0) THEN
+        SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = 'Password can not be empty.';
+    END IF; 
+    # Reject empty usernames
+    If (LENGTH(NEW.username) = 0) THEN
+        SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = 'Username can not be empty.';
+    END IF; 
+    # The email address must be valid
+    IF(NEW.email NOT REGEXP '^[[:alnum:]]+@[[:alpha:]]+[[.full-stop.]][[:alpha:]]{2,3}$') THEN
+        SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = 'Email is not valid.';
+    END IF;
+END;
+
+# Ensure validity
+CREATE PROCEDURE validateUser(password
+CREATE TRIGGER NewUser BEFORE INSERT ON Users
+FOR EACH ROW
+BEGIN 
+    # Reject empty passwords
+    If (LENGTH(NEW.password) = 0) THEN
+        SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = 'Password can not be empty.';
+    END IF; 
+    # Reject empty usernames
+    If (LENGTH(NEW.username) = 0) THEN
+        SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = 'Username can not be empty.';
+    END IF; 
+    # The email address must be valid
+    IF(NEW.email NOT REGEXP '^[[:alnum:]]+@[[:alpha:]]+[[.full-stop.]][[:alpha:]]{2,3}$') THEN
+        SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = 'Email is not valid.';
+    END IF;
+END;
+//
+DELIMITER ;
