45b8d038df3625d2a1edfd9d2e2c81ea408e0f57
s24luo <simonluo732@hotmail.com>
2014-11-27 02:00:14 +0900

    Create Conversations

--- DatabaseInitializationScript.sql
+++ DatabaseInitializationScript.sql
@@ -21,3 +21,33 @@ CREATE TABLE User (
     username CHAR(20) NOT NULL UNIQUE
 );
 
+#Create Conversations Table
+CREATE TABLE `pairbasedchatroom`.`Conversations` (
+  `uuid` INT NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC));
+  
+#Add foreign key
+ALTER TABLE `pairbasedchatroom`.`Conversations` 
+ADD COLUMN `topics.uuid` INT NOT NULL AFTER `uuid`,
+ADD COLUMN `positionAUser.uuid` INT NOT NULL AFTER `topics.uuid`,
+ADD COLUMN `positionBUser.uuid` INT NOT NULL AFTER `positionAUser.uuid`,
+ADD INDEX `topics.uuid_idx` (`topics.uuid` ASC),
+ADD INDEX `positionAUser.uuid_idx` (`positionAUser.uuid` ASC),
+ADD INDEX `positionBUser.uuid_idx` (`positionBUser.uuid` ASC);
+ALTER TABLE `pairbasedchatroom`.`Conversations` 
+ADD CONSTRAINT `topics.uuid`
+  FOREIGN KEY (`topics.uuid`)
+  REFERENCES `pairbasedchatroom`.`Topics` (`uuid`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `positionAUser.uuid`
+  FOREIGN KEY (`positionAUser.uuid`)
+  REFERENCES `pairbasedchatroom`.`Users` (`uuid`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `positionBUser.uuid`
+  FOREIGN KEY (`positionBUser.uuid`)
+  REFERENCES `pairbasedchatroom`.`Users` (`uuid`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
