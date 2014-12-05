8f4c09882c222fa435e520589121970b666f8541
jhorbulyk <jhorbulyk@inbox.com>
2014-12-05 18:17:43 +0900

    Add session support.

--- node/app.js
+++ node/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'ssshhhhh'}));
 
 app.use('/', routes);
 app.use('/users', users);
