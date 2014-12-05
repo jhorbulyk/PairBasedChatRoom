10a5a5f431f89452e3db4ced0460e593ea47e490
jhorbulyk <jhorbulyk@inbox.com>
2014-12-05 18:34:04 +0900

    Add DB connection.

--- node/app.js
+++ node/app.js
@@ -58,5 +58,22 @@ app.use(function(err, req, res, next) {
     });
 });
 
+// Set up DB connection
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    user     : 'root',
+    password : 'tester',
+    database : 'PairBasedChatRoom'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+});
+
 
 module.exports = app;
