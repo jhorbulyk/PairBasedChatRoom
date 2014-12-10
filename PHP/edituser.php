e0989a732e5f3ec19389288d3aa184b2ba2ae98b
jhorbulyk <jhorbulyk@inbox.com>
2014-12-10 20:00:23 +0900

    Add "Edit user functionality"

---
+++ PHP/edituser.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'sqlconnect.php';
+$conn = connectDB();
+$sql = $conn->prepare('UPDATE Users SET username=?,email=?,password=? WHERE id=?');
+
+include 'session.php';
+$sql->bind_param('sssd', $_POST["username"],$_POST["email"],$_POST["password"],getUser());
+$sql->execute();
+
+if($sql->error) {
+    echo $sql->error;
+} else {
+    echo "User updated successfully.";
+}
+
+$conn->close();
+
+?>
