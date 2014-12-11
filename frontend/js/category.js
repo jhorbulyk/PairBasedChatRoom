6d33d113e8d12ec427af0783864746b69d7fde6d
Dennis So <dennis.so@hotmail.com>
2014-12-11 00:12:30 -0800

    updates

--- frontend/js/category.js
+++ frontend/js/category.js
@@ -39,9 +39,9 @@ function createBreadcrumb(id, name, enable) {
 function createList(obj, id, name, enable, type) {
   if (type == "topic") {
     if (enable == true) {
-      obj.prepend("<a href=chat.html?id=" + id + " class=\"list-group-item\">" + name + "<span class=\"badge\">" + type + "</span></a>");
+      obj.prepend("<a href=chat.html?id=" + id + " class=\"list-group-item\">" + name + "<span class=\"badge\">" + type + "</span><div class=\"list-group-item-text\"><form id=\"join-conversation\" action=\"/chatroom/php/conversations/createconversation.php\" method=\"post\"><input type=\"hidden\"  name=\"topic\" value=\""+ id +"\"/><div class=\"btn-group\" role=\"group\" aria-label=\"...\"><button type=\"submit\" name=\"side\" value=\"A\" class=\"btn btn-default\">A</button><button type=\"submit\" name=\"side\" value=\"B\" class=\"btn btn-default\">B</button></div></form></div></a>");
     } else {
-      obj.prepend("<a href=?chat.html=" + id + " class=\"list-group-item disabled\">" + name + "<span class=\"badge\">" + type + "</span></a>");
+      obj.prepend("<a href=?chat.html=" + id + " class=\"list-group-item disabled\">" + name + "<span class=\"badge\">" + type + "</span><div class=\"list-group-item-text\"><form action=\"\"><div class=\"btn-group\" role=\"group\" aria-label=\"...\"><button type=\"button\" class=\"btn btn-default\">A</button><button type=\"button\" class=\"btn btn-default\">B</button></div></form></div></a>");
     }
   } else {
     if (enable == true) {
--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -1,5 +1,53 @@
+$(function () {
+  var params = getSearchParameters();
+  var id = (typeof params.id === 'undefined' || !isNumber(params.id)) ? 0 : params.id;
+  $.cookie("id", id);
+  var parent = $.cookie("parent");
+
+  $.post("/chatroom/php/category/breadcrumbtrail.php", {
+    parent: parent
+  }, function (data) {
+    var data = JSON.parse(data);
+    $.each(data, function (index) {
+      // leaf is index 0
+      if (index == -99) {
+        createBreadcrumb(data[index].id, data[index].name, false);
+      } else {
+        createBreadcrumb(data[index].id, data[index].name, true);
+      }
+    })
+  });
+})
+
 function sendMsg() {
   var text = $(".input-group > input").val();
   $(".chatbox").append('<li class = "media"><a class="media-left" href="#"><img src="http://dummyimage.com/64x64/000/fff" alt="..." ></a><div class="media-body"><h4 class="media-heading">Dennis So</h4><div class="md"><p>' + text + '</p></div><ul class="flat-list buttons"><li class="report-button"><a href="javascript:void(0)" class="action-thing" data-action-form="#report-action-form">report</a></li></ul></div></li>');
   $('.chatbox').scrollTop(1E10);
+}
+
+function createBreadcrumb(id, name, enable) {
+  if (enable == true) {
+    $(".breadcrumb").prepend("<li><a href=category.html?parent=" + id + ">" + name + "</a></li>");
+  } else {
+    $(".breadcrumb").prepend("<li class=category.html?parent=" + id + ">" + name + "</a></li>");
+  }
+}
+
+function getSearchParameters() {
+  var prmstr = window.location.search.substr(1);
+  return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray(prmstr) {
+  var params = {};
+  var prmarr = prmstr.split("&");
+  for (var i = 0; i < prmarr.length; i++) {
+    var tmparr = prmarr[i].split("=");
+    params[tmparr[0]] = tmparr[1];
+  }
+  return params;
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
