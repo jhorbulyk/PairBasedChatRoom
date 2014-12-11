403a2a44afacdf5bdae4054971588e82a1c53082
jhorbulyk <jhorbulyk@inbox.com>
2014-12-11 17:26:21 +0900

    start.

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -14,11 +14,10 @@ $(function () {
 })
 
 function createList(obj, content, postedByA) {
-    obj.prepend("<div class=\"list-group-tiem\"
     if (postedByA) {
       obj.prepend("<div" + " class=\"list-group-item\">" + content + "</div>");
     } else {
-      obj.prepend("<div" + " class=\"list-group-item\ disabled">" + content + "</div>");
+      obj.prepend("<div" + " class=\"list-group-item disabled\">" + content + "</div>");
     }
 }
 
@@ -26,4 +25,19 @@ function sendMsg() {
   var text = $(".input-group > input").val();
   $(".chatbox").append('<li class = "media"><a class="media-left" href="#"><img src="http://dummyimage.com/64x64/000/fff" alt="..." ></a><div class="media-body"><h4 class="media-heading">Dennis So</h4><div class="md"><p>' + text + '</p></div><ul class="flat-list buttons"><li class="report-button"><a href="javascript:void(0)" class="action-thing" data-action-form="#report-action-form">report</a></li></ul></div></li>');
   $('.chatbox').scrollTop(1E10);
+}
+
+function getSearchParameters() {
+  var prmstr = window.location.search.substr(1);
+  return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray(prmstr) {
+  var params = {};
+  var prmarr = prmstr.split("&");
+  for (var i = 0; i < prmarr.length; i++) {
+    var tmparr = prmarr[i].split("=");
+    params[tmparr[0]] = tmparr[1];
+  }
+  return params;
 }
